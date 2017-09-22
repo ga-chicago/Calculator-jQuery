ac8fd5688f07321b77d8f208e96160a8b6d8af6a
Todd herskovitz <toddherskovitz@Todds-MacBook-Pro.local>
2017-09-21 21:15:05 -0500

    addition

--- js/js.js
+++ js/js.js
@@ -2,13 +2,14 @@ console.log('working');
 
 //load
 	for (i = 0; i <= 9; i++) {
-		const square = $('<div>');// = $('<div>').on('click', (e) => {		});
+		const number = i;
+		const square = $('<div>').on('click', (e) => {	number_add(number);	});
 		square.text(i);
 		$(".buttons").append(square);
 		square.css("background-color", "pink");
 	}
 
-		const plus = $('<div>');// = $('<div>').on('click', (e) => {		});
+		const plus = $('<div>').on('click', (e) => {	add();	});
 		plus.text("+");
 		$(".buttons2").append(plus);
 		plus.css("background-color", "pink");
@@ -32,4 +33,46 @@ console.log('working');
 		equals.text("=");
 		$(".buttons2").append(equals);
 		equals.css("background-color", "pink");
+
+		let display = $('<div>');// = $('<div>').on('click', (e) => {		});
+		display.text("00000000");
+		$(".number").append(display);
+		display.css("background-color", "pink");
+
+let state = 'clear';
+let number_array1 = [];
+let number_array2 = 0;
+
+let argument = "";
+
+
+const number_add = (number) => {
+number_array1.push(number);
+
+display.text(number_array1.join(""));
+}
+
+
+const add = (number) => {
+number_array2 = parseInt(number_array1.join("")) + number_array2
+number_array1 = [];
+display.text(number_array2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
